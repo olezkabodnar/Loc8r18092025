966f7abf9ac882a7c9ac0f8437455551b51048c7
Student Oleh Bodnar (TL_KCOMP_B_Y3) <t00259052@mymtu.ie>
2025-09-18 10:33:01 +0100

    First commit

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,18 @@
 /* GET home page */
+
+
 const index = function(req, res){ 
 res.render('index', { title: 'Express' }); 
 };
+
+
+const login = function(req, res){ 
+res.render('login', { title: 'Express' }); 
+};
+
+
+
 module.exports = { 
 index 
+
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const ctrlMain = require('../controllers/main'); 
 /* GET home page. */
 router.get('/', ctrlMain.index); 
+
+//router.post('/register', ctrlMain.register)
+
 module.exports = router;
 
