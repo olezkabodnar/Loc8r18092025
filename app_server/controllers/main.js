d21cedee5dea980fb6248b7bab6ea4c199c4e92b
Oleh Bodnar <oleh@letsbeam.ie>
2025-10-16 08:26:10 +0100

    lab4

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -6,12 +6,76 @@ res.render('index', { title: 'Express' });
 };
 
 
-const login = function(req, res){ 
-res.render('login', { title: 'Login' }); 
+const login = function(req, res){
+    res.render('login', {
+        title: 'Login - Loc8r',
+        pageHeader: {
+            title: 'Login'
+        },
+        formFields: {
+            email: {
+                label: 'Email address',
+                placeholder: 'Email',
+                type: 'email',
+                name: 'email'
+            },
+            password: {
+                label: 'Password',
+                placeholder: 'Password',
+                type: 'password',
+                name: 'password'
+            }
+        },
+        rememberMe: 'Remember me',
+        submitButton: 'Login',
+        footerText: "Don't have an account?",
+        footerLink: {
+            text: 'Register here',
+            url: '/register'
+        }
+    });
 };
 
-const register = function(req, res){ 
-res.render('register', { title: 'Register' }); 
+const register = function(req, res){
+    res.render('register', {
+        title: 'Register - Loc8r',
+        pageHeader: {
+            title: 'Register'
+        },
+        formFields: {
+            name: {
+                label: 'Name',
+                placeholder: 'Name',
+                type: 'text',
+                name: 'name'
+            },
+            email: {
+                label: 'Email address',
+                placeholder: 'Email',
+                type: 'email',
+                name: 'email'
+            },
+            password: {
+                label: 'Password',
+                placeholder: 'Password',
+                type: 'password',
+                name: 'password'
+            },
+            reenterPassword: {
+                label: 'Re-enter Password',
+                placeholder: 'Password',
+                type: 'password',
+                name: 'reenter-password'
+            }
+        },
+        rememberMe: 'Remember me',
+        submitButton: 'Register',
+        footerText: 'Already have an account?',
+        footerLink: {
+            text: 'Login here',
+            url: '/login'
+        }
+    });
 };
 
 
